e13fc9e4b16fee2b6a07254d85c3a68024975d14
booblik2022 <booblik2022@gmail.com>
2024-03-19 23:06:25 +0300

    перенос на другой комп

--- spider.py
+++ spider.py
@@ -1,4 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://hh.ru/search/vacancy?text=Fullstack&from=suggest_post&area=1&hhtmFrom=main&hhtmFromLabel=vacancy_search_line"
+#url = "https://hh.ru/search/vacancy?text=Fullstack&from=suggest_post&area=1&hhtmFrom=main&hhtmFromLabel=vacancy_search_line"
+
+#headers = {
+    #"Accept": "*/*",
+    #"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0"
+#}
+#req = requests.get(url, headers=headers)
+
+#src = req.text
+#print(src)
+
+with open("index.html", "w", encoding="utf-8") as file:
+    file.write(src)
